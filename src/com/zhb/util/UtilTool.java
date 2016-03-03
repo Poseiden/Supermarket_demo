988709eda44ca6a7473962fc2de73f4e3b90387a
bbos1994 <bbos1994@163.com>
2016-03-03 18:30:36 +0800

    对UtilTool增加一个转换为map的方法

--- src/com/zhb/util/UtilTool.java
+++ src/com/zhb/util/UtilTool.java
@@ -1,7 +1,9 @@
 package com.zhb.util;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by ton on 16-3-3.
@@ -14,7 +16,7 @@ public class UtilTool {
      * @param originalData 原始字符串
      * @return ‘商品条形码-数量’ 的字符串集合
      */
-    public static List<String> splitOriginalData(String originalData){
+    public static List<String> splitOriginalDataToList(String originalData){
         //先把首尾的【】去掉，之后以，号分割开
         String[] productData = originalData.substring(1,originalData.length()-1).split(",");
         List<String> productStr = new ArrayList<String>();
@@ -24,5 +26,26 @@ public class UtilTool {
         }
 
         return productStr;
+    }
+
+    /**
+     * 分割原始传入的字符串
+     * @param originalData
+     * @return key为商品条形码，value为商品数量
+     */
+    public static Map<String,Integer> splitOriginalDataToMap(String originalData){
+        String[] productData = originalData.substring(1,originalData.length()-1).split(",");
+        Map<String,Integer> countMap = new HashMap<String,Integer>();
+        for(String str:productData){
+            String[] data = str.substring(1, str.length()-1).split("-");    //若有-，那么第一个元素是条形码，第二个元素是数量
+
+            if(countMap.containsKey(data[0])){
+                countMap.put(data[0],countMap.get(data[0])+(data.length==1?1:Integer.parseInt(data[1])));
+            }else{
+                countMap.put(data[0],data.length == 1 ?1:Integer.parseInt(data[1]));
+            }
+        }
+
+        return countMap;
     }
 }
